07fdeac8ec4efa04e36b01412fc22dcf0527a92e
USUARIO-PC\USUARIO <oscarruina@hotmail.com>
2020-08-13 21:03:56 -0300

    Firts Controller
    Update first controller

--- PrimerProyectoSpring/src/main/java/com/prueba/PrimerProyectoSpring/PrimerProyectoSpringApplication.java
+++ PrimerProyectoSpring/src/main/java/com/prueba/PrimerProyectoSpring/PrimerProyectoSpringApplication.java
@@ -1,13 +1,13 @@
-package com.prueba.ProyectoSpringWeb;
+package com.prueba.PrimerProyectoSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoSpringWebApplication {
+public class PrimerProyectoSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoSpringWebApplication.class, args);
+		SpringApplication.run(PrimerProyectoSpringApplication.class, args);
 	}
 
 }
